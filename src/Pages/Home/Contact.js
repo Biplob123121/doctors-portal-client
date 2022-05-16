e8d046b061087211db70c72e1495a7b49f573d87
Biplob123121 <biplobkb123@gmail.com>
2022-05-16 15:06:35 +0600

    completed contact section

--- src/Pages/Home/Contact.js
+++ src/Pages/Home/Contact.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import bgImage from '../../assets/images/appointment.png';
 
 const Contact = () => {
     return (
-        <div>
-            
-        </div>
+        <section style={{
+            background: `url(${bgImage})`
+        }} className='my-28'>
+            <div className=' w-1/2 mx-auto py-8 text-center'>
+                <div>
+                <h3 className='text-xl font-bold text-primary uppercase'>Contact us</h3>
+                <h2 className="text-3xl text-white">Stay connected with Us</h2>
+                </div>
+                <form className='block'>
+                    <input type="text" name='name' placeholder="Your Name" className="input w-full max-w-lg my-4" />
+                    <input type="email" name='email' placeholder="Email" className="input w-full max-w-lg" />
+                    <textarea type="email" name='email' placeholder="Description" className="input w-full my-4 max-w-lg" />
+                    <br />
+                    <button className="btn btn-primary uppercase text-white font-bold bg-gradient-to-r from-secondary to-primary">Submit </button>
+                </form>
+            </div>
+        </section>
     );
 };
 
