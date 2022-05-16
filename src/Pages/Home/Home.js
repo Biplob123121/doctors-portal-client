729c8869224c2c66f19af71929639183ef1bd08a
Biplob123121 <biplobkb123@gmail.com>
2022-05-16 14:30:28 +0600

    completed testimonials section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from './Banner';
+import Contact from './Contact';
 import ExceptionalService from './ExceptionalService';
 import Info from './Info';
 import MakeAppointment from './MakeAppointment';
@@ -15,6 +16,7 @@ const Home = () => {
             <ExceptionalService></ExceptionalService>
             <MakeAppointment></MakeAppointment>
             <Testimonials></Testimonials>
+            <Contact></Contact>
         </section>
     );
 };
--- src/Pages/Home/Testimonials.js
+++ src/Pages/Home/Testimonials.js
@@ -1,10 +1,52 @@
 import React from 'react';
+import quote from '../../assets/icons/quote.svg';
+import person1 from '../../assets/images/people1.png';
+import person2 from '../../assets/images/people2.png';
+import person3 from '../../assets/images/people3.png';
+import Review from './Review';
 
 const Testimonials = () => {
+    const reviews = [
+        {
+            _id: 1,
+            name: 'Winson Herry',
+            description: '',
+            image: person1,
+            location : "Dhaka"
+        },
+        {
+            _id: 2,
+            name: 'Winson Herry',
+            description: '',
+            image: person2,
+            location : "Dhaka"
+        },
+        {
+            _id: 3,
+            name: 'Winson Herry',
+            description: '',
+            image: person3,
+            location : "Dhaka"
+        },
+    ]
     return (
-        <div>
-            
-        </div>
+        <section>
+            <div className='flex justify-between'>
+                <div>
+                    <h3 className="text-xl text-primary font-bold">Testimonials</h3>
+                    <h2 className="text-3xl">What Our Patients Say</h2>
+                </div>
+                <img src={quote} className='w-24 lg:w-48' alt="" />
+            </div>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                {
+                    reviews.map(review => <Review
+                        key={review._id}
+                        review={review}
+                    ></Review>)
+                }
+            </div>
+        </section>
     );
 };
 
